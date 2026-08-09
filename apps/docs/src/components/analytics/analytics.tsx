e97bcbf230358c118f27f64f449a86603c99bc2d
Hakim Saoudi <saoudihakim@gmail.com>
2026-08-09 04:51:09 +0200

    fix(docs): serve Umami scripts through first-party routes

--- apps/docs/src/components/analytics/analytics.tsx
+++ apps/docs/src/components/analytics/analytics.tsx
@@ -4,27 +4,26 @@ import { env } from '@env'
 import Script from 'next/script'
 
 export function Analytics() {
-
-
     return (
         <>
-        <Script
-            defer
-            src={`${env.NEXT_PUBLIC_UMAMI_URL}/loader.js`}
-            data-website-id={env.NEXT_PUBLIC_UMAMI_ID}
-            data-performance="true"
-            strategy="afterInteractive"
+            <Script
+                defer
+                src="/p.js"
+                data-host-url={env.NEXT_PUBLIC_UMAMI_URL}
+                data-website-id={env.NEXT_PUBLIC_UMAMI_ID}
+                data-performance="true"
+                strategy="afterInteractive"
             />
-        <Script
-            defer
-            src={`${env.NEXT_PUBLIC_UMAMI_URL}/recorder.js`}
-            data-website-id={env.NEXT_PUBLIC_UMAMI_ID}
-            data-sample-rate="0.15" 
-            data-mask-level="moderate" 
-            data-max-duration="300000"
-            strategy="afterInteractive"
+            <Script
+                defer
+                src="/r.js"
+                data-host-url={env.NEXT_PUBLIC_UMAMI_URL}
+                data-website-id={env.NEXT_PUBLIC_UMAMI_ID}
+                data-sample-rate="0.15"
+                data-mask-level="moderate"
+                data-max-duration="300000"
+                strategy="afterInteractive"
             />
         </>
-        
     )
 }
